d63df0feeed99a3967457dde9c586bdfd8bfb7eb
João Vitor <jvc.lopes@outlook.com>
2024-08-12 10:40:39 -0300

    Special move - Promotion

--- src/app/Program.java
+++ src/app/Program.java
@@ -39,6 +39,12 @@ public class Program {
 				if(capturedPiece != null) {
 					captured.add(capturedPiece);
 				}
+				
+				if(chessMatch.getPromoted() != null) {
+					System.out.print("Enter Piece for promotion (B/N/R/T): ");
+					String type = sc.nextLine();
+					chessMatch.replacePromotedPiece(type);
+				}
 			}
 			catch (ChessException e) {
 				System.out.println(e.getMessage());
